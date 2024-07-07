e48e497d545a92c8fc276c257687bc226cffea5b
lore <106222990+LorieAnn26@users.noreply.github.com>
2024-07-07 23:31:47 +0800

    tanginang inventory

--- inventory.sql
+++ inventory.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 02, 2023 at 02:38 PM
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 08, 2024 at 04:10 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -47,18 +48,10 @@ CREATE TABLE `order_product` (
 --
 
 INSERT INTO `order_product` (`id`, `supplier`, `product`, `quantity_ordered`, `quantity_received`, `quantity_remaining`, `status`, `batch`, `created_by`, `requested_by`, `created_at`, `updated_at`) VALUES
-(25, 7, 33, 32, 9, 23, 'incomplete', 1682655197, 31, '', '2023-04-28 06:13:17', '2023-04-28 06:13:17'),
-(26, 7, 32, 12, 6, 6, 'incomplete', 1682655207, 31, '', '2023-04-28 06:13:27', '2023-04-28 06:13:27'),
-(27, 8, 32, 32, 8, 24, 'incomplete', 1682655207, 31, '', '2023-04-28 06:13:27', '2023-04-28 06:13:27'),
-(28, 8, 31, 143, 143, 0, 'complete', 1682656750, 31, '', '2023-04-28 06:39:10', '2023-04-28 06:39:10'),
-(29, 8, 37, 0, NULL, NULL, 'pending', 1700628019, 31, '', '2023-11-22 05:40:19', '2023-11-22 05:40:19'),
-(30, 8, 36, 32, NULL, NULL, 'pending', 1700628080, 31, '', '2023-11-22 05:41:20', '2023-11-22 05:41:20'),
-(31, 8, 36, 32, NULL, NULL, 'pending', 1700628601, 31, 'Test 1', '2023-11-22 05:50:01', '2023-11-22 05:50:01'),
-(32, 7, 32, 322, NULL, NULL, 'pending', 1700628601, 31, 'Test 2', '2023-11-22 05:50:01', '2023-11-22 05:50:01'),
-(33, 8, 32, 12, NULL, NULL, 'pending', 1700628601, 31, 'Test 2', '2023-11-22 05:50:01', '2023-11-22 05:50:01'),
-(34, 8, 35, 32, 52, -20, 'complete', 1700711098, 31, 'Angelo', '2023-11-23 04:44:58', '2023-11-23 04:44:58'),
-(35, 8, 36, 32, 32, 0, 'complete', 1701517119, 31, '', '2023-12-02 12:38:39', '2023-12-02 12:38:39'),
-(36, 8, 37, 23, 23, 0, 'complete', 1701517119, 31, '', '2023-12-02 12:38:39', '2023-12-02 12:38:39');
+(13, 4, 3, 3, 3, 0, 'complete', 1707884042, 41, 'Jane', '2024-02-14 05:14:02', '2024-02-14 05:14:02'),
+(14, 4, 2, 32, 32, 0, 'complete', 1707884042, 41, 'John', '2024-02-14 05:14:02', '2024-02-14 05:14:02'),
+(15, 5, 2, 23, 23, 0, 'complete', 1707884042, 41, 'John', '2024-02-14 05:14:02', '2024-02-14 05:14:02'),
+(16, 4, 9, 32, 32, 0, 'complete', 1711205068, 41, '', '2024-03-23 15:44:28', '2024-03-23 15:44:28');
 
 -- --------------------------------------------------------
 
@@ -79,17 +72,11 @@ CREATE TABLE `order_product_history` (
 --
 
 INSERT INTO `order_product_history` (`id`, `order_product_id`, `qty_received`, `date_received`, `date_updated`) VALUES
-(21, 26, 2, '2023-04-27 06:13:56', '2023-04-27 06:13:56'),
-(22, 27, 4, '2023-04-27 06:13:56', '2023-04-27 06:13:56'),
-(23, 25, 2, '2023-04-26 06:14:13', '2023-04-26 06:14:13'),
-(24, 26, 4, '2023-04-28 06:14:51', '2023-04-28 06:14:51'),
-(25, 27, 4, '2023-04-25 06:14:51', '2023-04-25 06:14:51'),
-(26, 25, 4, '2023-04-28 06:15:16', '2023-04-28 06:15:16'),
-(27, 25, 3, '2023-04-28 06:15:22', '2023-04-28 06:15:22'),
-(28, 28, 143, '2023-04-28 06:39:29', '2023-04-28 06:39:29'),
-(29, 36, 23, '2023-12-02 12:38:57', '2023-12-02 12:38:57'),
-(30, 35, 32, '2023-12-02 12:38:57', '2023-12-02 12:38:57'),
-(31, 34, 52, '2023-12-02 14:32:00', '2023-12-02 14:32:00');
+(5, 14, 23, '2024-02-14 05:14:41', '2024-02-14 05:14:41'),
+(6, 13, 3, '2024-02-14 05:14:41', '2024-02-14 05:14:41'),
+(7, 15, 23, '2024-02-14 05:14:41', '2024-02-14 05:14:41'),
+(8, 14, 9, '2024-02-14 05:15:31', '2024-02-14 05:15:31'),
+(9, 16, 32, '2024-03-23 15:44:41', '2024-03-23 15:44:41');
 
 -- --------------------------------------------------------
 
@@ -114,12 +101,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product_name`, `description`, `img`, `price`, `stock`, `created_by`, `created_at`, `updated_at`) VALUES
-(31, 'Milo', 'This is an energy drink											\r\n										', 'product-1682655126.png', '32.00', 0, 31, '2023-04-28 06:12:06', '2023-04-28 06:12:06'),
-(32, 'Kitkat', 'This is a chocolate', 'product-1682655158.png', '12.50', 1, 31, '2023-04-28 06:12:38', '2023-04-28 06:12:38'),
-(33, 'Nescafe2', '  This is a coffee bean											<br />										', 'product-1682656653.png', '3.25', 4, 31, '2023-04-28 06:12:59', '2023-04-28 06:12:59'),
-(35, 'Kopiko', 'Brown Cofee is real											\r\n										', 'product-1700326972.png', '5.00', 52, 31, '2023-11-18 18:02:52', '2023-11-18 18:02:52'),
-(36, 'Nike Bag', '  This is a nike bag.											<br />										', 'product-1700327171.png', '21.00', 0, 31, '2023-11-18 18:05:19', '2023-11-18 18:05:19'),
-(37, 'Ring', 'this is a ring											\r\n										', 'product-1700327185.png', '32.00', 23, 31, '2023-11-18 18:06:25', '2023-11-18 18:06:25');
+(2, 'Kopiko Blanco', ' Energy drink											<br />										', 'product-1708923614.png', '21.10', 21, 41, '2024-02-14 05:11:44', '2024-02-14 05:11:44'),
+(3, 'Nescafe', ' Coffee								', 'product-1708923638.png', '71.10', 0, 41, '2024-02-14 05:12:09', '2024-02-14 05:12:09'),
+(4, 'MIlo', 'Energy drink											\r\n										', 'product-1708923691.png', '31.00', 0, 41, '2024-02-26 06:01:31', '2024-02-26 06:01:31'),
+(5, 'Nido', 'This is milk											\r\n										', 'product-1708923706.png', '25.00', 16, 41, '2024-02-26 06:01:46', '2024-02-26 06:01:46'),
+(6, 'Coke Zero', 'This is coke											\r\n										', 'product-1708923872.png', '21.00', 0, 41, '2024-02-26 06:04:32', '2024-02-26 06:04:32'),
+(7, 'Coke Mismo', 'Coke mismo											\r\n										', 'product-1708923908.png', '45.00', 30, 41, '2024-02-26 06:05:08', '2024-02-26 06:05:08'),
+(9, 'Toblerone Chocolate', 'Tasty											\r\n										', 'product-1711205047.png', '32.52', 0, 41, '2024-03-23 15:44:07', '2024-03-23 15:44:07');
 
 -- --------------------------------------------------------
 
@@ -140,13 +128,14 @@ CREATE TABLE `productsuppliers` (
 --
 
 INSERT INTO `productsuppliers` (`id`, `supplier`, `product`, `updated_at`, `created_at`) VALUES
-(40, 8, 31, '2023-04-28 06:12:06', '2023-04-28 06:12:06'),
-(41, 8, 32, '2023-04-28 06:12:38', '2023-04-28 06:12:38'),
-(42, 7, 32, '2023-04-28 06:12:38', '2023-04-28 06:12:38'),
-(45, 7, 33, '2023-04-28 06:37:33', '2023-04-28 06:37:33'),
-(47, 8, 35, '2023-11-18 18:02:52', '2023-11-18 18:02:52'),
-(50, 8, 36, '2023-11-18 18:06:11', '2023-11-18 18:06:11'),
-(51, 8, 37, '2023-11-18 18:06:25', '2023-11-18 18:06:25');
+(6, 4, 2, '2024-02-26 06:00:14', '2024-02-26 06:00:14'),
+(7, 5, 2, '2024-02-26 06:00:14', '2024-02-26 06:00:14'),
+(8, 4, 3, '2024-02-26 06:00:38', '2024-02-26 06:00:38'),
+(9, 4, 4, '2024-02-26 06:01:31', '2024-02-26 06:01:31'),
+(10, 4, 5, '2024-02-26 06:01:46', '2024-02-26 06:01:46'),
+(11, 5, 6, '2024-02-26 06:04:32', '2024-02-26 06:04:32'),
+(12, 5, 7, '2024-02-26 06:05:08', '2024-02-26 06:05:08'),
+(14, 4, 9, '2024-03-23 15:44:07', '2024-03-23 15:44:07');
 
 -- --------------------------------------------------------
 
@@ -160,19 +149,10 @@ CREATE TABLE `requests` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) DEFAULT NULL,
   `date` date DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
   `updated_at` datetime NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `requests`
---
-
-INSERT INTO `requests` (`id`, `requested_by`, `product_id`, `quantity`, `date`, `updated_at`, `created_at`) VALUES
-(7, 'Test', 36, 3, '2023-12-06', '2023-12-02 11:35:11', '2023-12-02 11:35:11'),
-(8, 'Jane', 33, 2, '2023-12-28', '2023-12-02 11:41:53', '2023-12-02 11:41:53'),
-(9, 'Kitkat', 32, 3, '2023-12-14', '2023-12-02 12:31:07', '2023-12-02 12:31:07'),
-(10, 'John Doe', 36, 32, '2023-12-30', '2023-12-02 14:21:32', '2023-12-02 14:21:32');
 
 -- --------------------------------------------------------
 
@@ -195,8 +175,8 @@ CREATE TABLE `suppliers` (
 --
 
 INSERT INTO `suppliers` (`id`, `supplier_name`, `supplier_location`, `email`, `created_by`, `created_at`, `updated_at`) VALUES
-(7, 'Nestle', 'United States', 'supplier@nestle.com', 31, '2023-04-28 06:10:39', '2023-04-28 06:10:39'),
-(8, 'Robinson', 'United Kingdom', 'supplier@robinson.com', 31, '2023-04-28 06:11:29', '2023-04-28 06:11:29');
+(4, 'Nestle', 'PHI', 'nestle@supplier.com', 41, '2024-02-14 05:10:30', '2024-02-14 05:10:30'),
+(5, 'Coca-cola', 'PHI', 'supplier@cocacola.com', 41, '2024-02-14 05:10:45', '2024-02-14 05:10:45');
 
 -- --------------------------------------------------------
 
@@ -210,6 +190,7 @@ CREATE TABLE `users` (
   `last_name` varchar(50) NOT NULL,
   `password` varchar(2000) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `permissions` varchar(5000) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -218,9 +199,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `password`, `email`, `created_at`, `updated_at`) VALUES
-(31, 'John2', 'Doe2', '$2y$10$cjv7hqlOlwjs7xGpxZmm/e3LXqokrY4wexxQt0nVIfzP6q0X3UGKG', 'johndoe@yahoo.com', '2023-04-28 06:05:49', '2023-04-28 06:40:21'),
-(33, 'Jan', 'Tan', '$2y$10$2KSKRzDp1SEMf4q01ISIK.SlJRQ8DHlAxw29sy0vaC4Y9.Y3WOSc.', 'janjantandayag123@gmail.com', '2023-11-08 12:05:34', '2023-11-08 12:05:34');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `password`, `email`, `permissions`, `created_at`, `updated_at`) VALUES
+(41, 'Admin', 'Admin', '$2y$10$KhhP6aVuB1Cf4O.6fXDMOORrLJF14jdmtdRMXjW281Jni5G/proyG', 'admin@ims.com', 'dashboard_view,report_view,po_view,product_view,pos,user_view,supplier_view,po_create,product_create,supplier_create,user_create,supplier_edit,product_edit,po_edit,user_edit,supplier_delete,product_delete,user_delete', '2024-01-21 05:30:45', '2024-01-31 07:07:52'),
+(43, 'John', 'Doe', '$2y$10$caPK3HLqiUqcUiNhEysYC.kztR5Yg.gJh8sBTrMkFfCbdpsDZ7OEK', 'johndoe@yahoo.com', 'po_view,product_view,supplier_view,user_view,pos,po_create,product_create,supplier_create,user_create,po_edit,product_edit,user_edit,supplier_edit', '2024-03-10 14:55:23', '2024-03-11 05:57:10');
 
 --
 -- Indexes for dumped tables
@@ -285,86 +266,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `order_product`
 --
 ALTER TABLE `order_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `order_product_history`
 --
 ALTER TABLE `order_product_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `productsuppliers`
 --
 ALTER TABLE `productsuppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `order_product`
---
-ALTER TABLE `order_product`
-  ADD CONSTRAINT `order_product_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`),
-  ADD CONSTRAINT `order_product_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
-  ADD CONSTRAINT `order_product_ibfk_3` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `order_product_history`
---
-ALTER TABLE `order_product_history`
-  ADD CONSTRAINT `order_product_history_ibfk_1` FOREIGN KEY (`order_product_id`) REFERENCES `order_product` (`id`);
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `fk_user` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `productsuppliers`
---
-ALTER TABLE `productsuppliers`
-  ADD CONSTRAINT `productsuppliers_ibfk_1` FOREIGN KEY (`product`) REFERENCES `products` (`id`),
-  ADD CONSTRAINT `productsuppliers_ibfk_2` FOREIGN KEY (`supplier`) REFERENCES `suppliers` (`id`);
-
---
--- Constraints for table `requests`
---
-ALTER TABLE `requests`
-  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
-
---
--- Constraints for table `suppliers`
---
-ALTER TABLE `suppliers`
-  ADD CONSTRAINT `fk_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
