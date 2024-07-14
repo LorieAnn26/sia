acdb1fd43d733450ad2ae2f20261d9cc7e56812b
lore <106222990+LorieAnn26@users.noreply.github.com>
2024-07-14 22:52:10 +0800

    pfp update

--- partials/app-sidebar.php
+++ partials/app-sidebar.php
@@ -5,7 +5,7 @@
 <div class="dashboard_sidebar" id="dashboard_sidebar">
 	<h3 class="dashboard_logo" id="dashboard_logo">LPG</h3>
 	<div class="dashboard_sidebar_user">
-		<img src="images/user/sample.jpg" alt="User image." id="userImage" />
+		<img src="images/user/sample.png" alt="User image." id="userImage" />
 		<span><?= $user['first_name'] . ' ' . $user['last_name'] ?></span>
 	</div>
 	<div class="dashboard_sidebar_menus">
